f7a31441119c0b624f696a0687edbb62fb7cf76c
Emilia Folta <emilkafolta@gmail.com>
2023-02-04 21:52:10 +0000

    Added vuetify 3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,14 +2,14 @@
 import NavBar from "./components/NavBar.vue";
 import SignIn from "./components/SignIn.vue";
 import { useAuthStore } from "./stores/auth";
-import UserView from "./views/UserView.vue";
 useAuthStore().initAuth();
+
 </script>
 
 <template>
   <div
     v-if="useAuthStore().userType"
-    class="dark flex min-h-screen flex-col dark:bg-slate-800"
+    class="text-white flex min-h-screen flex-col bg-slate-800"
   >
     <NavBar />
     <RouterView />
