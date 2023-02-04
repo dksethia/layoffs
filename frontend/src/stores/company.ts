46f6ad245928e1df0c1de780557f7392d5c792d8
Shilen <sp1420@ic.ac.uk>
2023-02-04 23:34:37 +0000

    Merged companies

--- frontend/src/stores/company.ts
+++ frontend/src/stores/company.ts
@@ -9,32 +9,26 @@ export const useCompanyStore = defineStore("company", () => {
   function getCompany(companyID: string) {
     // fetch company from backend
     company.value = {
-<<<<<<< Updated upstream
-        id: "1",
-        logoUrl: "logourl",
-        email: "example@example.com",
-        recruited: 10,
-        sustainabilityScore: 50,
-        inclusivityScore: 60,
-        description: "description",
-        website: "website",
-    }
+      id: "1",
+      logoUrl: "logourl",
+      email: "example@example.com",
+      recruited: 10,
+      sustainabilityScore: 50,
+      inclusivityScore: 60,
+      description: "description",
+      website: "website",
+    };
   }
 
   function getRoles() {
     // fetch jobs for company
     return [
-        { id: "1", name: "Machine Learning Engineer", location: "Berlin" },
-        { id: "2", name: "Product Manager", location: "Berlin" },
-        { id: "3", name: "Senior Software Engineer", location: "London" },
-        { id: "4", name: "Cloud Engineer", location: "Birmingham" },
-        { id: "5", name: "DevOps Engineer", location: "Bristol" },
-      ]
-=======
-      roles: [
-      ],
-    };
->>>>>>> Stashed changes
+      { id: "1", name: "Machine Learning Engineer", location: "Berlin" },
+      { id: "2", name: "Product Manager", location: "Berlin" },
+      { id: "3", name: "Senior Software Engineer", location: "London" },
+      { id: "4", name: "Cloud Engineer", location: "Birmingham" },
+      { id: "5", name: "DevOps Engineer", location: "Bristol" },
+    ];
   }
 
   function addRole(role: Role) {
