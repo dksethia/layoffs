3786806d415bdf0ebde431c6f149cab4bb8dd8f6
dksethia <41707087+dksethia@users.noreply.github.com>
2023-02-04 20:16:21 +0000

    add roleview to homeview

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { useAuthStore } from "@/stores/auth";
 import UserView from "./UserView.vue";
+import RoleView from "@/components/company/RoleView.vue";
 </script>
 
 <template>
     <UserView v-if="useAuthStore().isUser()"/>
+    <RoleView v-else />
 </template>
