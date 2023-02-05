dbf13fa69f662ca5d43510e2fc09df80f3e5c3b9
Shilen <sp1420@ic.ac.uk>
2023-02-05 00:40:29 +0000

    fixed merged issue

--- frontend/src/views/UserView.vue
+++ frontend/src/views/UserView.vue
@@ -91,7 +91,7 @@ const selectedCompanies = computed(() =>
     <div class="flex flex-col items-center grow">
       <!-- Main part of the website -->
       <div class="w-3/5 m-2 p-4 rounded-lg">
-        <label for="email-address" class="sr-only">Search</label>
+        <label class="sr-only">Search</label>
         <input
           id="search"
           v-model="role"
@@ -101,41 +101,18 @@ const selectedCompanies = computed(() =>
       </div>
       <div
         v-for="c in selectedCompanies"
-        class="w-3/5 shadow-lg flex justify-between bg-[#1a1c23] text-white m-2 p-5 rounded-lg"
+        class="w-4/5 shadow-lg flex justify-between bg-[#1a1c23] text-white m-2 p-5 rounded-lg"
+        @click="dialog = true"
       >
         <div class="flex flex-col justify-around">
           <div class="font-bold text-xl">{{ c.name }}</div>
           <div class="text-sm">{{ c.description }}</div>
         </div>
-        <PieChart :p="c.sustainabilityScore * 100" />
-        <!-- Main part of the website -->
-        <div class="w-3/5 bg-slate-800 m-2 p-4 rounded-lg">
-          <label for="email-address" class="sr-only">Search</label>
-          <input
-            v-model="role"
-            id="role"
-            name="role"
-            class="relative block w-full appearance-none rounded-lg border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-            placeholder="Search your dream job..."
-          />
+        <div>
+          Sustainability Score:
+          <PieChart :p="c.sustainabilityScore * 100" />
         </div>
-        <div
-          v-for="c in selectedCompanies"
-          class="w-3/5"
-          @click="dialog = true"
-        >
-          <div class="w-full bg-slate-800 m-2 p-4 rounded-lg">
-            <div class="font-bold">{{ c.name }}</div>
-            <div>{{ c.description }}</div>
-            <div>
-              Sustainability Score:
-              <PieChart :p="c.sustainabilityScore * 100" />
-            </div>
-            <div>
-              Inclusivity Score: <PieChart :p="c.sustainabilityScore * 100" />
-            </div>
-          </div>
-        </div>
+        <div>Inclusivity Score: <PieChart :p="c.inclusivityScore * 100" /></div>
         <div class="text-center">
           <v-dialog v-model="dialog">
             <v-card>
