70a615089c32f629ddcadbd530deb458dc048a45
Shilen <sp1420@ic.ac.uk>
2023-02-05 05:07:46 +0000

    Update user profile style

--- frontend/src/components/user/UserProfile.vue
+++ frontend/src/components/user/UserProfile.vue
@@ -9,7 +9,7 @@ const user = {
   formerCompany: "Google",
   formerRole: "Owner",
   experience: 27,
-  linkedin: "lorenzo.linkedin.com",
+  linkedin: "https://www.lorenzo.linkedin.com",
   password: "ichack",
   profileSummary: "An ex CS Oxford student who got laid off from Google :(",
   location: "UK",
@@ -19,19 +19,11 @@ const user = {
   race: "white",
   isDisabled: true,
 };
-
-// // Fetching the data in real time
-// const user = ref(null)
-// fetch("http://localhost:3000/api/user")
-//     .then((response) => response.json())
-//     .then((data) => {
-//         user.value = data;
-//     });
 </script>
 <template>
   <div class="flex justify-center p-5 gap-5">
-    <div class="flex flex-col">
-      <div>
+    <div class="flex flex-col bg-[#1a1c23] p-8 rounded-[40px] shadow-lg">
+      <div class="flex justify-center">
         <v-avatar
           size="20em"
           image="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
@@ -39,53 +31,55 @@ const user = {
       </div>
       <div>{{ user.firstName.toUpperCase() }}</div>
       <div class="text-2xl font-bold">{{ user.lastName.toUpperCase() }}</div>
+      <div class="text-2xl text-[#fff5]">{{ user.gender }}</div>
       <div class="mt-5">
         {{ user.profileSummary }}
       </div>
+      <div class="flex justify-around mt-5">
+        <a
+          :href="user.linkedin"
+          target="_blank"
+          class="bg-purple-300 hover:bg-purple-200 text-black p-3 rounded"
+          >Linked in</a
+        >
+        <a
+          :href="`mailto:${user.email}`"
+          class="bg-purple-300 hover:bg-purple-200 text-black p-3 rounded"
+          >Email</a
+        >
+      </div>
     </div>
-    <div class="w-1/2">
-      <v-text-field
-        disabled
-        :model-value="user.email"
-        variant="outlined"
-        label="Email"
-      />
-      <v-text-field
-        disabled
-        :model-value="user.linkedin"
-        variant="outlined"
-        label="Linkedin"
-      />
-      <v-text-field
-        disabled
-        :model-value="user.formerCompany"
-        variant="outlined"
-        label="Former Company"
-      />
-      <v-text-field
-        disabled
-        :model-value="user.formerRole"
-        variant="outlined"
-        label="Most recent role"
-      />
-      <v-text-field
-        disabled
-        :model-value="user.location"
-        variant="outlined"
-        label="Lives in"
-      />
-      <v-text-field
-        disabled
-        :model-value="user.remote ? 'Yes!' : 'Unfortunately not'"
-        variant="outlined"
-        label="Open for remote working"
-      />
-      <v-text-field
-        disabled
-        :model-value="user.gender"
-        variant="outlined"
-        label="Gender"
-      />
+    <div class="w-1/2 flex flex-col gap-5 justify-between">
+      <div class="bg-[#1a1c23] p-8 rounded-[40px] shadow-lg">
+        <div class="text-2xl font-bold">Experience</div>
+        <div class="my-3">
+          <div class="font-bold">Former Company:</div>
+          <div>{{ user.formerCompany }}</div>
+        </div>
+        <div class="mt-3">
+          <div class="font-bold">Former Role:</div>
+          <div>{{ user.formerRole }}</div>
+        </div>
+      </div>
+      <div class="bg-[#1a1c23] p-8 rounded-[40px] shadow-lg">
+        <div class="text-2xl font-bold">Preferences</div>
+        <div class="my-3">
+          <div class="font-bold">Location:</div>
+          <div>{{ user.location }}</div>
+        </div>
+        <div class="my-3">
+          <div class="font-bold">Open for remote working:</div>
+          <div>{{ user.remote ? "Yes!" : "Unfortunately not" }}</div>
+        </div>
+        <div class="my-3">
+          <div class="font-bold">Former Company:</div>
+          <div>{{ user.formerCompany }}</div>
+        </div>
+        <div class="mt-3">
+          <div class="font-bold">Former Role:</div>
+          <div>{{ user.formerRole }}</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
