ffe7384c55174efaba134649fb2981f9c854412b
Shilen <sp1420@ic.ac.uk>
2023-02-05 02:16:12 +0000

    Made some buttons accessible

--- frontend/src/components/company/CandidateCard.vue
+++ frontend/src/components/company/CandidateCard.vue
@@ -1,32 +1,34 @@
 <script setup lang="ts">
-import LinkedIn from "@/assets/LinkedIn.vue"
+import LinkedIn from "@/assets/LinkedIn.vue";
 
 const props = defineProps(["candidate"]);
-
-function log() {
-  console.log("click")
-}
 </script>
 
-<!-- <template>
-  <div class="bg-slate-500 rounded my-3 p-3 cursor-pointer">
-    <h2 class="font-bold text-xl">{{ `${props.candidate.firstName} ${props.candidate.lastName}` }}</h2>
-    <p> {{ `ex-${props.candidate.formerRole}, ${props.candidate.formerCompany}` }} </p>
-    <p> {{ `${props.candidate.experience} years of experience` }} </p>
-    <v-btn :href="props.candidate.linkedin" target="_blank" :icon="LinkedIn"></v-btn>
-    <v-btn :href="`mailto:${props.candidate.email}`" >Email</v-btn>
-    
-  </div>
-</template> -->
-
 <template>
-    <v-card>
-      <v-card-title> {{ `${props.candidate.firstName} ${props.candidate.lastName}` }} </v-card-title>
-      <v-card-subtitle>{{ `ex-${props.candidate.formerRole}, ${props.candidate.formerCompany}` }}</v-card-subtitle>
-      <v-card-subtitle>{{ `${props.candidate.experience} years of experience` }}</v-card-subtitle>
-      <v-card-actions>
-          <v-btn :href="props.candidate.linkedin" target="_blank" :icon="LinkedIn"></v-btn>
-          <v-btn :href="`mailto:${props.candidate.email}`" >Email</v-btn>
-      </v-card-actions>
-    </v-card>
+  <div class="flex justify-between bg-[#1a1c23] m-5 p-5 rounded items-center">
+    <div>
+      <div class="font-bold text-2xl">
+        {{ `${props.candidate.firstName} ${props.candidate.lastName}` }}
+      </div>
+      <div>
+        {{
+          `ex-${props.candidate.formerRole}, ${props.candidate.formerCompany}`
+        }}
+      </div>
+      <div>
+        {{ `${props.candidate.experience} years of experience` }}
+      </div>
+    </div>
+    <div class="flex flex-col gap-4 items-center mt-5">
+      <a :href="props.candidate.linkedin" target="_blank">
+        <LinkedIn />
+      </a>
+      <a
+        :href="`mailto:${props.candidate.email}`"
+        class="bg-gray-800 px-3 py-1 rounded hover:bg-gray-700"
+      >
+        Email
+      </a>
+    </div>
+  </div>
 </template>
--- frontend/src/components/company/RoleCard.vue
+++ frontend/src/components/company/RoleCard.vue
@@ -3,7 +3,7 @@ const props = defineProps(["role"]);
 </script>
 
 <template>
-  <div class="bg-slate-900 rounded my-3 p-3 cursor-pointer">
+  <div class="bg-[#1a1c23] rounded my-3 p-3 cursor-pointer hover:bg-[#1f1f2d]">
     <h2 class="font-bold text-xl">{{ props.role.name }}</h2>
     <p>{{ props.role.location }}</p>
   </div>
